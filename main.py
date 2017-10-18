5087f26eee2565e3f56baa540df03717d2a4e83e
Zareef Ali <eeyore93@ymail.com>
2017-10-18 22:04:13 +0100

    Update with hijri calendar and tafseer cogs

--- main.py
+++ main.py
@@ -1,22 +1,37 @@
 import discord
 from discord.ext import commands
 
-description = 'A Discord bot which allows you to search ahadith from sunnah.com.'
+description = "A bot with Qu'ran, hadith, hijri calendar and prayer time functions. Bot owner: @Zaify#6850"
 
-bot = commands.Bot(command_prefix='.', description=description)
+bot = commands.Bot(command_prefix='-', description=description)
+servers = len(bot.servers)
+
 
 # Setup
 @bot.event
 async def on_ready():
-    print('Logged in as')
-    print(bot.user.name)
-    print(bot.user.id)
+    print(f'Logged in as {bot.user.name}')
+    await bot.change_presence(game=discord.Game(name=f'-help on {servers} servers'))
 
-    await bot.change_presence(game=discord.Game(name='-hadith to search ahadith'))
 
 # Load cogs
-bot.load_extension('hadith')
-bot.load_extension('prayertimes')
-bot.load_extension('quran')
+cog_list = ['hadith', 'prayertimes', 'quran', 'hijricalendar', 'tafsir']
+for cog in cog_list:
+    bot.load_extension(cog)
+
+
+# Reload cog command for owner
+@bot.command(pass_context=True, hidden=True)
+async def reload(ctx):
+    if ctx.message.author.id == '184402067685638144':
+        for cog in cog_list:
+            bot.unload_extension(cog)
+            print(f'Unloading {cog}')
+        for cog in cog_list:
+            bot.load_extension(cog)
+            print(f'Loading {cog}')
+        await bot.say('Reload complete!')
+    else:
+        await bot.say('Only @Zaify#6850 can reload the bot.')
 
 bot.run('tokenhere')

9e7635acb36a88dc107d64fa4966fccb7f33f768
NegativeScript <ala2aldeen913@gmail.com>
2017-10-19 18:40:04 +0300

    refactored the tafsir method. added indirection. lots of indirection.

--- tafsir.py
+++ tafsir.py
@@ -5,12 +5,32 @@ from utils import makeEmbed
 
 icon = 'https://lh3.ggpht.com/zoyAL6BWpiHrgyFEujQcEXhBqZn4SfX0JiIFqOecs2JoZYy39Yam8xiz7Vq6kP7S2w=w300'
 
+
+class TafsirError(Exception):
+    pass
+
+
+class TafsirSpecifics:
+    def __init__(self, surah, ayah, maxayah):
+        self.surah = surah
+        self.minAyah = ayah
+        self.maxAyah = maxayah
+        self.tafsir = ''
+        self.orderedDict = OrderedDict()
+
+    def unpack(self):
+        return self.surah, self.minAyah, self.maxAyah, self.tafsir, self.orderedDict
+
+
 tafsir_list = ['jalalayn', 'muyassar']
+
+
 class Tafsir:
 
     def __init__(self, bot):
         self.bot = bot
         self.session = ClientSession(loop=bot.loop)
+        self.url = 'http://api.globalquran.com/ayah/{}:{}/{}'
 
     @commands.bot.command(pass_context=True)
     async def tafsir(self, ctx, ref: str, tafsir: str = None):
@@ -19,96 +39,90 @@ class Tafsir:
         sender = ctx.message.author
         server = ctx.message.server
         serverid = ctx.message.server.id
+
         print(f'{sender} executed command tafsir on {server} ({serverid})')
 
-        # Get surah
-        surah = int(ref.split(':')[0])
+        try:
+            surah, min_ayah, max_ayah = self.processRef(ref)
+            tafsirSpec = TafsirSpecifics(surah, min_ayah, max_ayah)
+        except:
+            await self.bot.say("Invalid arguments! Do `-tafsir [surah]:[ayah] (optional tafsir name)`. "
+                               "Example: `-tafsir 1:1`"
+                               "\n\n"
+                               "To quote multiple verses, do `-tafsir [surah]:[first ayah]-[last ayah]`"
+                               "\n\n"
+                               "Example 2: `-tafsir 1:1-7 muyassar`"
+                               "\n\n"
+                               f"**Valid editions**: `{tafsir_list}`")
+            return
 
-        # Get first ayah
+        if tafsir is None:
+            tafsir = 'ar.jalalayn'
+            await self.bot.say('Defaulting to Tafsir al-Jalalayn.')
+        else:
+            try:
+                tafsir = self.processTafsirName(tafsir)
+            except TafsirError:
+                await self.bot.say(f'Invalid tafsir. Valid tafsirs are: `{tafsir_list}`')
+                return
+        tafsirSpec.tafsir = tafsir
+
+        self.getTafsirs(tafsirSpec)
+        readableTafsir = self.getReadableTafsirName(tafsir)
+
+        em = makeEmbed(fields=tafsirSpec.orderedDict, author=readableTafsir, author_icon=icon, colour=0x467f05,
+                       inline=False)
+
+        await self.bot.say(embed=em)
+
+    @staticmethod
+    def processRef(ref):
+
+        surah = int(ref.split(':')[0])
         min_ayah = int(ref.split(':')[1].split('-')[0])
 
-        # Get last ayah
         try:
             max_ayah = int(ref.split(':')[1].split('-')[1]) + 1
         except IndexError:
             max_ayah = min_ayah + 1
 
-        # Otherwise throw an error
-        except:
-            await self.bot.say("Invalid arguments! Do `-tafsir [surah]:[ayah] (optional tafsir name)`. Example: `-tafsi"
-                               "r 1:1`\n\nTo quote multiple verses, do `-tafsir [surah]:[first ayah]-[last ayah]`\n\nEx"
-                               "ample 2: `-tafsir 1:1-7 muyassar`"
-                               f"\n\n**Valid editions**: `{tafsir_list}`")
-            return
+        return [surah, min_ayah, max_ayah]
 
-        # No tafsir specified? Default to Tafsir al-Jalalayn
-        if tafsir is None:
-            tafsir = 'ar.jalalayn'
-            await self.bot.say('Defaulting to Tafsir al-Jalalayn.')
-
-        # Valid tafsir? Just add 'ar.' before the edition name to convert it to the name needed for API
-        elif tafsir in tafsir_list:
+    @staticmethod
+    def processTafsirName(tafsir):
+        if tafsir in tafsir_list:
             tafsir = 'ar.' + tafsir
-
-        # Otherwise, give the invalid edition error message.
         else:
-            await self.bot.say(f'Invalid tafsir. Valid tafsirs are: `{tafsir_list}`')
-            return
+            raise TafsirError
 
-        # Text needs to be in an OrderedDict for some reason
-        o = OrderedDict()
+        return tafsir
 
-        # Set base URL
-        url = 'http://api.globalquran.com/ayah/{}:{}/{}'
+    @staticmethod
+    def getReadableTafsirName(tafsir):
+        tafsirDict = {
+            'ar.jalalayn': 'Tafsir al-Jalalayn',
+            'ar.muyassar': 'Tafsir al-Muyassar',
+        }
+        return tafsirDict[tafsir]
 
-        # Get data from API
-        async with self.session.get(url.format(surah, min_ayah, tafsir)) as r:
+    async def getTafsirs(self, tafsirspec):
+        surah, minAyah, maxAyah, tafsir, orDict = tafsirspec.unpack()
+
+        for verse in range(minAyah, maxAyah):
+            self.getTafsir(orDict, tafsir, surah, verse)
+
+    async def getTafsir(self, orDict, tafsir, surah, verse):
+        async with self.session.get(self.url.format(surah, verse, tafsir)) as r:
             data = await r.json()
 
-        # Get tafsir text
-
-        # Stage 1: Load the JSON and list values
         for page in data["quran"][f'{tafsir}'].values():
 
-            # Stage 2: Search for the 'verse' key and assign it to the text variable
             text = page["verse"]
-
-            '''
-            If we input an invalid ayah number for surah (e.g 1:1000), it will just carry onto the next
-            surahs, so in case this happens, get the actual surah and ayah number from the page.
-            '''
-
             surah = page["surah"]
             ayah = page["ayah"]
 
-            # Stage 3: Convert from unicode numbers to characters we can read
             text = u"{}".format(text)
-            o[f'{surah}:{ayah}'] = text
-
-        # If multiple ayahs need to be quoted, get the tafsir text for them
-        for verse in range(min_ayah + 1, max_ayah):
-            async with self.session.get(url.format(surah, verse, tafsir)) as r:
-                data = await r.json()
-
-                # Get tafsir text (as above)
-                for page in data["quran"][f'{tafsir}'].values():
-                    text = page["verse"]
-                    text = u"{}".format(text)
-
-                    surah = page["surah"]
-                    ayah = page["ayah"]
-                    o['{}:{}'.format(surah, verse)] = text
-
-        # Format tafsir name
-        tafsir = {
-            'ar.jalalayn': 'Tafsir al-Jalalayn',
-            'ar.muyassar': 'Tafsir al-Muyassar',
-        }[tafsir]
-
-        # Construct and send the embed
-        em = makeEmbed(fields=o, author=tafsir, author_icon=icon, colour=0x467f05,
-                       inline=False)
-        await self.bot.say(embed=em)
+            orDict[f'{surah}:{ayah}'] = text
 
 
 # Register as cog
